3b8d28ee7bccd7689cc4ac2823c2d8fa830246eb
kbutkus@gmail.com <kbutkus@gmail.com>
2019-10-01 03:14:53 +0000

    first commit

---
+++ Fibonacci.rb
@@ -0,0 +1,35 @@
+def iterative_fib(number)
+array = []
+counter = number
+a = 0
+b = 1
+c = 0
+print "#{a}, "
+  until counter == 0
+    c = a + b
+    a = b
+    b = c
+    print "#{c}, "
+    counter = counter - 1
+  end
+puts 
+end
+
+@array = [0,1]
+def recursive_fib(number)
+  return number if number < 2
+
+  @array[number] = recursive_fib(number - 1) + recursive_fib(number - 2)
+
+end
+
+# n = 35
+# puts "#{n}'s fibonacci value is #{recursive_fib(n)}"
+# puts iterative_fib(n)
+
+require 'benchmark'
+num = 35
+Benchmark.bm do |x|
+  x.report("recursive_fib") { recursive_fib(num) }
+  x.report("iterative_fib") { iterative_fib(num) }
+end
